2e62b0628c35ff0460575d2c0ead59b54254aa45
swiftpint <swiftpintx1@protonmail.com>
2022-04-24 15:42:48 +0000

    bacup

--- index.js
+++ index.js
@@ -11,14 +11,15 @@ app.get('/', async (req, res) => {
     // let location = "areaType=region&areaCode=E12000007"
     let location = "E09000023"
     try {
-        let data = await getData(location, 1);
-        console.log(data.clean[0])
-
+        let data = await getData(location);
+        console.log(data.clean[0].length)
+        console.log("sending data")
         res.render("index", {data: JSON.stringify(data)});
     }
     catch (err){
         console.log(err)
-        res.send("error")
+        console.log("ERROR")
+        res.send(err)
     }
 });
 
@@ -47,7 +48,7 @@ app.get('/location', async (req, res) => {
 const port = 3001;
 app.listen(port, () => console.log('Corona App listening on port', port));
 
-const getData = async (location, dateType) => {
+const getData = async (location) => {
     console.log(location)
     let data = []
 
@@ -68,40 +69,42 @@ const getData = async (location, dateType) => {
     //     // "newCasesByPublishDateAgeDemographics"
     // ]    
 
-    const dataFields = [
-        [
+    const structure = [
+        // [
             "cumDeathsByDeathDate",
             "newCasesByPublishDateRollingRate",
             "newCasesByPublishDateRollingSum",
             "newCasesBySpecimenDateRollingRate",
-            "newCasesBySpecimenDateChangePercentage",
-            // "newCasesByPublishDateAgeDemographics"
-        ],
+            "newCasesByPublishDateAgeDemographics",
+            "newCasesByPublishDate",
+            "newCasesBySpecimenDate",
+        // ],
         // [
-        //     "alertLevel",
-        //     "newCasesByPublishDate",
-        //     "newCasesBySpecimenDate",
-        //     "newDeathsByDeathDate",
-        //     "newDeathsByPublishDate",
+            // "alertLevel",
+            // "newCasesByPublishDate",
+            // "newCasesBySpecimenDate",
+            // "newDeathsByDeathDate",
+            // "newDeathsByPublishDate",
         // ]
     ]
-    console.log(dataFields.length) 
-    dataFields.forEach(async function(structure){
-    console.log("for each", "s:", structure, "l:", location, "d:", dateType)
-        let res = await fetchData(structure, location, dateType)
-        console.log("res", res)
-        data.push(...res)
-    })
+    // console.log(dataFields.length) 
+    // dataFields.forEach(async function(structure){
+        // console.log("for each", "s:", structure, "l:", location, "d:", dateType)
+        let res = await fetchData(structure, location)
+        // console.log("res", res)
+        // console.log("res", res.data)
+        data.push(...res.data)
+    // })
     console.log(data)
-    data.clean = await cleanData(data.body, dateType)
-    console.log(data.clean[0])
-    data.summary = await summary(data);
+    data.clean = await cleanData(data.body)
+    // console.log(data.clean[0])
+    // data.summary = await summary(data);
     return data;
 };  // getData
 
 
-async function fetchData(structure, location, dateType){
-    console.log(structure, location, dateType)
+async function fetchData(structure, location){
+    console.log(structure, location)
     let structureString = "";
     structure.forEach(function(e){
         structureString += "metric" + "=" + e + "&";
@@ -109,7 +112,8 @@ async function fetchData(structure, location, dateType){
     structureString = structureString.trim("&");
     // location = "E06000029"
     const url = (
-        "https://api.coronavirus.data.gov.uk/v2/data?" + 
+        // "https://api.coronavirus.data.gov.uk/v1/data?" + 
+        "https://api.coronavirus.data.gov.uk/v1/data?" + 
         "areaType=ltla&areaCode=" + location + "&" + structureString + "format=json"
     );
     console.log(url);
@@ -133,111 +137,133 @@ async function summary(data){
     let rollingRate;
     let alertLevelValue;
     let alertLevelMsg;
-    for (let i = 0; i < data["body"].length; i++){
+    // for (let i = 0; i < data["body"].length; i++){
 
-        if (typeof data["body"][i]["newCasesBySpecimenDateRollingRate"] === "number"){
-            rollingRate = data["body"][i]["newCasesBySpecimenDateRollingRate"];
-            break;
-            // return {"rollingRate": rollingRate}
-        }
-    }
-    for (let i = 0; i < data["body"].length; i++){
-        // console.log("here")
-        if (typeof data["body"][i]["alertLevel"] === "number"){    
-            // console.log("here2")    
-            alertLevelValue = data["body"][i]["alertLevelValue"];
-            alertLevelMsg = data["body"][i]["alertLevelName"];
-            break;
-        }
-    }
+    //     if (typeof data["body"][i]["newCasesBySpecimenDateRollingRate"] === "number"){
+    //         rollingRate = data["body"][i]["newCasesBySpecimenDateRollingRate"];
+    //         break;
+    //         // return {"rollingRate": rollingRate}
+    //     }
+    // }
+    // for (let i = 0; i < data["body"].length; i++){
+    //     // console.log("here")
+    //     if (typeof data["body"][i]["alertLevel"] === "number"){    
+    //         // console.log("here2")    
+    //         alertLevelValue = data["body"][i]["alertLevelValue"];
+    //         alertLevelMsg = data["body"][i]["alertLevelName"];
+    //         break;
+    //     }
+    // }
     // console.log(rollingRate)
     return {
         areaName: data["body"][0]["areaName"],
-        rollingRate: rollingRate,
-        alertLevelValue: alertLevelValue,
-        alertLevelMsg: alertLevelMsg,
+        // rollingRate: rollingRate,
+        // alertLevelValue: alertLevelValue,
+        // alertLevelMsg: alertLevelMsg,
         areaCode: data["body"][0]["areaCode"]
     };
 }
 
-function cleanData(data, dateType){
-    console.log(data[0])
+function cleanData(data){
+    // console.log(data[0])
 
-    let tempAlertLevel = 6;
-    let tempAlertMsg = "National Lockdown";    
+    // let tempAlertLevel = 6;
+    // let tempAlertMsg = "National Lockdown";    
     
     let cleanData = data.reduce( (tot, curr, index, src) => {
-        let casesSpecDate;
+        // let casesSpecDate;
         let cases;
         let cumDeaths;
         let deaths;
         // let sevenDayAverage;
-        let rollingRateSpecDate;
+        // let rollingRateSpecDate;
         let rollingRate;
         let alertLevel;
         let alertMsg;
-
+        let firstVacDaily;
+        let firstVacCum;
+        let secVacDaily;
+        let secVacCum;
         // Check if AlertLevel is null in the data or not
-        if (curr.alertLevel === null || curr.alertLevel === -99) {
-            let i = index -1;
-            alertLevel = tempAlertLevel;
-            alertMsg = tempAlertMsg;             
-        } else {
-            alertLevel = curr.alertLevelValue;
-            alertMsg = curr.alertLevelName;
-            tempAlertLevel = curr.alertLevelValue;
-            tempAlertMsg = curr.alertLevelName;         
-        }
+        // if (curr.alertLevel === null || curr.alertLevel === -99) {
+        //     let i = index -1;
+        //     alertLevel = tempAlertLevel;
+        //     alertMsg = tempAlertMsg;             
+        // } else {
+        //     alertLevel = curr.alertLevelValue;
+        //     alertMsg = curr.alertLevelName;
+        //     tempAlertLevel = curr.alertLevelValue;
+        //     tempAlertMsg = curr.alertLevelName;         
+        // }
 
         // Cases - 1 = specimen date and 2 = publish date
-        if (dateType == 1){
-            cases = curr.newCasesBySpecimenDate;  
-            deaths = curr.newDeathsByDeathDate;
 
-            if (curr.newCasesBySpecimenDateRollingRate === null){
-                rollingRate = 0;
-            } else {
-                rollingRate = curr.newCasesBySpecimenDateRollingRate;   
-            }
+        // if (dateType == 1){
+            // cases = curr.casesDaily;
+            // deaths = curr.deathsDaily;
+            // cases = curr.casesDaily;  
+            // deaths = curr.newDeathsByDeathDate;
 
-        } else {
-            cases = curr.newCasesByPublishDate;    
-            deaths = curr.newDeathsByPublishDate;
-            if (curr.newCasesByPublishDateRollingRate === null){
-                rollingRate = 0;
-            } else {
-                rollingRate = curr.newCasesByPublishDateRollingRate;
-            }
-        }
+            // if (curr.newCasesBySpecimenDateRollingRate === null){
+            //     rollingRate = 0;
+            // } else {
+            //     rollingRate = curr.newCasesBySpecimenDateRollingRate;   
+            // }
+
+        // } else {
+        //     cases = curr.newCasesByPublishDate;    
+        //     deaths = curr.newDeathsByPublishDate;
+        //     if (curr.newCasesByPublishDateRollingRate === null){
+        //         rollingRate = 0;
+        //     } else {
+        //         rollingRate = curr.newCasesByPublishDateRollingRate;
+        //     }
+        // }
 
         // Daily Deaths
-        if (curr.newDeathsByDeathDate === null){
+        if (curr.deathsDaily === null){
             deaths = 0;
         } else {
-            deaths = curr.newDeathsByDeathDate;
+            deaths = curr.deathsDaily;
+        }
+
+        // Daily Cases
+        if (curr.casesDaily === null){
+            cases = 0;
+        } else {
+            cases = curr.casesDaily;
         }
 
         // Cumulative Deaths
-        if (curr.cumDeathsByDeathDate === null){
+        if (curr.deathsCumulative === null){
             cumDeaths = 0;
         } else {
-            cumDeaths = curr.cumDeathsByDeathDate;
+            cumDeaths = curr.deathsCumulative;
         }
 
+        // Daily Cases
+        if (curr.casesCumulative === null){
+            cumCases = 0;
+        } else {
+            cumCases = curr.casesCumulative;
+        }
         let data = {
             date: curr.date,
             cases: cases,
+            cumCases: cumCases,
             deaths: deaths,
             cumDeaths: cumDeaths,
-            rollingRate: rollingRate,
-            alertLevel: alertLevel,
-            alertMsg: alertMsg
+            // rollingRate: rollingRate,
+            // alertLevel: alertLevel,
+            // alertMsg: alertMsg
         }
-
-        if ( cases + cumDeaths + deaths + rollingRate === 0){
-        } else {
-            tot.push(data)            
-        }
+        console.log(data)
+        tot.push(data) 
+        // if ( cases + cumDeaths + deaths === 0){
+        //     console.log(cases + cumDeaths + deaths)
+        // } else {
+        //     tot.push(data)            
+        // }
         return tot;
     },[])
     
@@ -249,8 +275,9 @@ function cleanData(data, dateType){
     });
     
     cleanData.reverse()
+    // Why do we do this?:
     cleanData[cleanData.length-1]["cumDeaths"] = cleanData[cleanData.length-2]["cumDeaths"]
     
-    console.log(cleanData[0])
+    console.log("clean data", cleanData[0])
     return cleanData;
 }
